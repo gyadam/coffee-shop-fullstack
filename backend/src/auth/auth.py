6fce50219f8db3d3c4ac9d78e4febd02b3a2b9c9
adam-ubuntu <gy.adam90@gmail.com>
2020-04-13 15:33:48 -0700

    Implement check_permissions()

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -36,19 +36,15 @@ def get_token_auth_header():
 
     return header_parts[1]
 
-'''
-@TODO implement check_permissions(permission, payload) method
-    @INPUTS
-        permission: string permission (i.e. 'post:drink')
-        payload: decoded jwt payload
 
-    it should raise an AuthError if permissions are not included in the payload
-        !!NOTE check your RBAC settings in Auth0
-    it should raise an AuthError if the requested permission string is not in the payload permissions array
-    return true otherwise
-'''
 def check_permissions(permission, payload):
-    raise Exception('Not Implemented')
+    if 'permissions' not in payload:
+        abort(400)
+    
+    if permission not in payload['permissions']:
+        abort(403)
+
+    return True
 
 '''
 @TODO implement verify_decode_jwt(token) method
