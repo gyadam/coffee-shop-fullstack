7e48fb2551af38645c17a27434eba5b02b7ef40b
adam-ubuntu <gy.adam90@gmail.com>
2020-04-14 17:48:11 -0700

    Remove completed TODOs

--- backend/src/api.py
+++ backend/src/api.py
@@ -88,9 +88,7 @@ def delete_drinks(jwt, drink_id):
 
 
 ## Error Handling
-'''
-Example error handling for unprocessable entity
-'''
+
 @app.errorhandler(422)
 def unprocessable(error):
     return jsonify({
@@ -99,16 +97,7 @@ def unprocessable(error):
                     "message": "unprocessable"
                     }), 422
 
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False, 
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
 
-'''
 @app.errorhandler(404)
 def not_found(error):
     return jsonify({
@@ -117,15 +106,7 @@ def not_found(error):
                     "message": "resource not found"
                     }), 404
 
-'''
-@TODO implement error handler for 404
-    error handler should conform to general task above 
-'''
 
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above 
-'''
 @app.errorhandler(AuthError)
 def authorization_error(error):
     return jsonify({
